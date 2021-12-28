d177f93abb8bb5200cc2cb3a9c2b55e6d88d7885
GuillaumeMZ <guillaume.merchez@etu.u-paris.fr>
2021-12-28 17:57:04 +0100

    Routing and templating

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import Fastify from 'fastify';
+import pointOfView from 'point-of-view';
+import Mustache from 'mustache';
+import axios from 'axios';
+import {Client} from 'pg';
+require('dotenv').config();
+
+const client = new Client();
+
+async function getUserInfos(userId: string) {
+    try {
+        const result = await axios.get(`https://discord.com/api/v8/users/${userId}`, {
+            headers: {
+                'authorization' : `Bot ${process.env.BOT_TOKEN}`,
+                'Content-Type' : 'application/json'
+            }
+        });
+
+        //le résultat est dans result.data
+        console.log(result.data);
+    }
+    catch(error) {
+        console.error('Erreur');
+    }
+}
+
+const server = Fastify({logger: true});
+server.register(pointOfView, {
+    engine: {
+        mustache: Mustache
+    }
+});
+
+interface guildParams {
+    guildId : string
+}
+
+server.get<{Params: guildParams}>('/:guildId', async (req, res) => {
+    const guildId = req.params.guildId;
+    return res.view('rank.mustache', {name: req.params.guildId});
+});
+
+const start = async () => {
+    await server.listen(8081);
+};
+
+start();
